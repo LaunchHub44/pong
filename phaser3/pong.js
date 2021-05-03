6375c348dc1e70e8da1741be61f04d1608d14033
LaunchHub44 <jakechung2222@gmail.com>
2021-05-02 20:52:25 -0400

    Created paddles and ball using Phaser.

--- phaser3/pong.js
+++ phaser3/pong.js
@@ -1,27 +1,45 @@
 var config = {
     type: Phaser.AUTO,
-    width: 200,
-    height: 200,
+    width: 400,
+    height: 300,
+    backgroundColor: 0x000000,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 },
+            enableBody: true,
+        }
+    },
     scene: {
         preload: preload,
         create: create,
         update: update
     },
-    fps: {
-        target: 1,
-        forceSetTimeOut: true
-    }
-};
+
+    // fps: {
+    //     target: 1,
+    //     forceSetTimeOut: true
+    // }
+
+}
+
 var game = new Phaser.Game(config);
 
 function preload() {
-    console.log("Hello, I'm preload function.")
+    this.load.audio('paddle-sound', 'audio/paddle-bounce.mp3')
+    this.load.audio('score-sound','audio/score.mp3')
+    this.load.audio('wall-bounce-sound', 'audio/wall-bounce.mp3')
+    this.load.image('paddle', 'image/paddle-white-10x60.png')
+    this.load.image('ball', 'image/ball.png')
 }
 
 function create() {
-    console.log("Hello, I'm create function.")
+    this.physics.add.sprite(20, 150, 'paddle')
+    this.physics.add.sprite(380, 150, 'paddle')
+    this.physics.add.sprite(200, 150, 'ball')
 }
 
+
 function update() {
-    console.log("Hello, I'm update function.")
+
 }
