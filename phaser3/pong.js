2b512b535f512c149a70d60b1bfade6f73ad6a26
LaunchHub44 <jakechung2222@gmail.com>
2021-05-06 20:18:16 -0400

    Made a scoring system.

--- phaser3/pong.js
+++ phaser3/pong.js
@@ -28,8 +28,12 @@ var config = {
 }
 
 gameState = {
-   
+    p1Score: 0,        // HIGH-LEVEL: declare variable, with initial value of 0, defined (definition)
+    p2Score: 0,
+    player1: null,     // HIGH-LEVEL: declare variable (no init value)
+    player2: null,
 }
+
 ballState = {
     x: 200,
     y: 150,
@@ -61,6 +65,9 @@ function create() {
     gameState.player1 = this.physics.add.sprite(P1_X, 150, 'paddle')
     gameState.player2 = this.physics.add.sprite(P2_X, 150, 'paddle')
 
+    gameState.p1ScoreText = this.add.text(100, 50, gameState.p1Score, { fontSize: '45px', fill: '#ffffff' });
+    gameState.p2ScoreText = this.add.text(300, 50, gameState.p2Score, { fontSize: '45px', fill: '#ffffff' });
+
     gameState.ball = this.physics.add.sprite(ballState.x, ballState.y, 'ball')
     for (var y = 0; y < 300; y += 20) {
         this.add.rectangle(200, y, 9, 9, '0x333333')
@@ -68,7 +75,7 @@ function create() {
 
     paddleSound = this.sound.add('paddle-sound')
     wallSound = this.sound.add('wall-bounce-sound')
-    //scoreSound = this.sound.add('score-sound')
+    scoreSound = this.sound.add('score-sound')
 
     // collision setup
     gameState.player1.setCollideWorldBounds(true)
@@ -89,6 +96,13 @@ function create() {
         console.log(`ball xspeed: ${ballState.xSpeed}`)
         setBallAngle()
     })
+}
+
+function ballReset() {
+    gameState.ball.x = 200
+    gameState.ball.y = 150
+    ballState.xSpeed = 100
+    ballState.ySpeed = 0
 }
 
 function update() {
@@ -125,4 +139,24 @@ function update() {
         wallSound.play()
         ballState.ySpeed = -ballState.ySpeed
     }
+
+    // Score Condition
+    if (gameState.ball.x < 0) {
+        scoreSound.play()
+        gameState.p2Score += 1
+        ballReset();
+
+        // Present Score
+        gameState.p2ScoreText.setText(gameState.p2Score)
+    }
+    
+    if (gameState.ball.x > 400) {
+        scoreSound.play()
+        gameState.p1Score += 1
+        ballReset();
+
+        // Present Score
+        gameState.p1ScoreText.setText(gameState.p1Score)
+    }
+
 }
